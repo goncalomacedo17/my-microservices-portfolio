1800d6d0212a2fc171d2a31418fb15b6d75c0d9a
goncalomacedo17 <joaog.macedo@hotmail.com>
2026-01-22 12:10:59 +0000

    initial commit

---
+++ inventoryService/src/test/java/com/example/goncalomacedo17/inventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goncalomacedo17.inventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
